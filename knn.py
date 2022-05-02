dc4c0b2688e6ccf6e8ec8b665d44800de23e7d50
Trevor Rizzo <tarizzo@wpi.edu>
2022-05-02 18:02:24 -0400

    add gitignore

--- knn.py
+++ knn.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     x, y = create_time_series_data('data/', samples_per_time_series, x_features, y_feature)
 
     x = list(x)
-    y = y.tolist()
+    y = y.toXlist()
 
     clf = KNNClassifier(k=1, classes=list(set(y)))
     clf.fit(x, y)
